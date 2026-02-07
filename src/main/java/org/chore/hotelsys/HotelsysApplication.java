d9f76725b454e30689cf0879dba34d5b2ce68531
cxw668 <2070873373@qq.com>
2026-02-07 21:24:24 +0800

    first commit

---
+++ src/main/java/org/chore/hotelsys/HotelsysApplication.java
@@ -0,0 +1,13 @@
+package org.chore.hotelsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsysApplication.class, args);
+    }
+
+}
